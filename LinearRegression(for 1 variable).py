50a53130e1ec20402b74d385bab4ea1d0e4c7f29
demin1078 <43730344+demin1078@users.noreply.github.com>
2021-11-12 11:33:51 +0300

    Create LinearRegression(for 1 variable).py

---
+++ LinearRegression(for 1 variable).py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import functools
+import statistics
+import math
+area = [100,110,115,120,135,170,173,190,220,250]
+price = [500, 530, 520, 550, 580,600,590,670,760,850]
+
+#print(sum([i**2 for i in area])/(2*len(area)))
+plt.plot(area,price,'go')
+#plt.show()
+
+def hypothesis(th0,th1,x1):
+    return th0 + th1*x1
+#It's a derivative, I was wrong
+def squareError(m, th0, th1, x, y,x0):
+    s = 0
+    if x0 == 1:
+        x0 = [1 for i in range(m)]
+    for i in range(m):
+        s += (hypothesis(th0, th1, x[i]) - y[i]) * x0[i]
+    return s/(2*m)
+def createLine(th0,th1,m):
+    c = m.copy()
+    arr = [th0+th1*i for i in c]
+    plt.plot(area,arr)
+    plt.show()
+
+th0 = 0
+th1 = 0
+a = 0.0001
+m = len(price)
+th0Past = 1000000
+th1Past = 1000000
+it = 0
+while abs(th1 - th1Past) > 0.00000001 and abs(th0 - th0Past) > 0.00000001:
+    th0Past = th0
+    th1Past = th1
+
+    temp0 = th0 - a * squareError(m,th0,th1,area,price,1)
+    temp1 = th1 - a * squareError(m,th0,th1,area,price,area)
+    th0 = temp0
+    th1 = temp1
+    #print(th0,th1)
+    it += 1
+print(th0,th1)
+createLine(th0,th1,area)
+
+
