1230d652d1ef1cf95548bf194255237068508178
demin1078 <43730344+demin1078@users.noreply.github.com>
2022-04-13 16:26:54 +0300

    Update pandas_for_analys.py

--- pandas_for_analys.py
+++ pandas_for_analys.py
@@ -6,3 +6,8 @@ s1 = pd.Series([1,10,3,np.nan], index=pd.to_datetime(['2022-01-01', '2022-01-03'
 s1 = s1.asfreq(freq = '1d', method = 'pad')
 s1.fillna(s1.shift(1),inplace =True )
 print(s1)
+
+
+#Fillna can also take dict or series and so it is possible to change different columns in different ways simultaneously  
+df = pd.DataFrame(data = {'user' : ['Ivan', 'Petr', 'Alex', 'Den'], 'user.speed' : [40, None, 42, 35], 'user.bag_weight' : [8, None, 2, 4]})
+print(df.fillna({'user.speed':df['user.speed'].mean(), 'user.bag_weight': df['user.bag_weight'].median()}))
