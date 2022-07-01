b020fc77ed6774e88106e6db83e6ba6cf8262f12
demin1078 <43730344+demin1078@users.noreply.github.com>
2022-07-01 14:27:15 +0300

    Update Helpfull_funct_for_ml.py

--- Helpfull_funct_for_ml.py
+++ Helpfull_funct_for_ml.py
@@ -1,5 +1,5 @@
 
-#Find correlation in data set
+#Find correlation in data set between all collumns. It finds ONLE linear corellation
 def find_correlation(data, min_cor,max_cor = 1.1 )
     columns = data.columns
     strong_correlation = []
@@ -9,3 +9,6 @@ def find_correlation(data, min_cor,max_cor = 1.1 )
             if subtle_cor > min_cor and i != j and subtle_cor < max_cor:
                 strong_correlation.append(f"{i} and {j} correlates in {subtle_cor}")
     return strong_correlation
+
+#Mutual correlation find ALL types of correlation between feuture and target
+
